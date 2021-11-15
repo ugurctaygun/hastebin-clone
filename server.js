ed9a236db7727cb1862ddc94a5e46c6bef363fd1
ugurctaygun <ugurctaygun@gmail.com>
2021-11-15 21:24:36 +0300

    mongoose - new

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/documents");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/hastebin-clone", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 const code = `# Haste
 
@@ -21,6 +29,31 @@ To make a new entry, click "New" (or type 'control + n')`;
 
 app.get("/", (req, res) => {
   res.render("code-display", { code });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    const newDocument = await Document.create({ value });
+    res.redirect(`/${newDocument.id}`);
+  } catch (error) {
+    res.render("new", { value });
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = Document.findById(id);
+    res.render("code-display", { code: document });
+    console.log(code);
+  } catch (error) {
+    res.redirect("/");
+  }
 });
 
 app.listen(3000);
